e0aeba1003cdb268e032d117648593b39c3fbb53
DethMetalDuck <jamesboorman95@hotmail.com>
2018-11-02 17:40:42 +0000

    Updated the way we use keys

---
+++ secrets.py
@@ -0,0 +1,19 @@
+# Database credentials
+# Put your username here (email that is associated with the github)
+# Packages - mysql-connector, discord, pycryptodome
+# The email will be reset the first time you try to connect, so use mysql workbench or something to connect and look at the database. THe password to login for the first time is password
+database_username = ''
+database = 'LUHack_Discord_Verification_Bot_Test_DB'
+database_port = 42069
+database_host = 'luhack-discord-verification-bot-database.cff2dtcegzwl.eu-west-2.rds.amazonaws.com'
+database_password = ''
+
+# Email credentials
+email_username = 'luhack.discord.verifier.test@gmail.com'
+email_password = 'H4tr3dC0pter'
+
+# Secret bot client token
+bot_client_token = 'NTAyOTQwODg5MTYxNjYyNDY1.DqvQyw.ZdewAkNVqze8_RrhLnriKqWpsLM'
+
+# Aes key for email encryption
+aes_encryption_key = 'A3n7UX9EkjP46RtfgDU9WdlZe'
