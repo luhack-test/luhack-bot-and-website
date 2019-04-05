5abf57f3bf1b6c2d3fd256bcb36800c6be6e482b
Ben Simms <ben@bensimms.moe>
2019-04-05 01:06:16 +0100

    Improve schema, More info below
    - add create/edit date to writeups
    - store username of users
    - proper on delete cascade stuff

--- luhack_bot/db/models.py
+++ luhack_bot/db/models.py
@@ -1,4 +1,8 @@
+import datetime
+
 from gino import Gino
+from sqlalchemy import func
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy_utils import EncryptedType
 from sqlalchemy_searchable import make_searchable
 from sqlalchemy_utils.types import TSVectorType
@@ -12,6 +16,7 @@ class User(db.Model):
     __tablename__ = "users"
 
     discord_id = db.Column(db.BigInteger(), primary_key=True)
+    username = db.Column(db.Text(), nullable=False)
     email = db.Column(EncryptedType(db.Text(), email_encryption_key), nullable=False)
 
 
@@ -19,11 +24,15 @@ class Writeup(db.Model):
     __tablename__ = "writeups"
 
     id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(None, db.ForeignKey("users.discord_id"), nullable=False)
+    author_id = db.Column(None, db.ForeignKey("users.discord_id", ondelete="CASCADE"), nullable=False)
+    author = relationship(User, backref=backref("writeups", passive_deletes=True))
 
     title = db.Column(db.Text(), nullable=False)
     tags = db.Column(db.ARRAY(db.Text()), nullable=False)
     content = db.Column(db.Text(), nullable=False)
+
+    creation_date = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+    edit_date = db.Column(db.DateTime, server_default=func.now(), server_onupdate=func.now(), nullable=False)
 
     search_vector = db.Column(
         TSVectorType("title", "content", weights={"title": "A", "content": "B"})
