7c1d27cb0f1fad8cfe5aafa65a5140ed0271bc8b
Ben Simms <ben@bensimms.moe>
2019-04-11 00:48:15 +0100

    Limit tag lengths

--- writeups_site/forms.py
+++ writeups_site/forms.py
@@ -1,6 +1,6 @@
 import re
 
-from wtforms import Form, StringField, TextAreaField, Field, validators
+from wtforms import Form, StringField, TextAreaField, Field, validators, ValidationError
 from wtforms.widgets import TextInput
 
 
@@ -8,13 +8,24 @@ class TagListField(Field):
     widget = TextInput()
 
     def _value(self):
-        return " ".join(self.data or ())
+        return ", ".join(self.data or ())
 
     def process_formdata(self, valuelist):
         if valuelist:
-            self.data = filter(lambda s: s and not s.isspace(), re.split(r'[^\w-]+', valuelist[0]))
+            data = filter(lambda s: s and not s.isspace(), re.split(r'[^\w-]+', valuelist[0].lower()))
+            data = list(set(data))
+            self.data = data
         else:
             self.data = []
+
+    def post_validate(self, form, validation_stopped):
+        for i in self.data:
+            l = len(i)
+            if l < 3 or l > 20:
+                raise ValidationError("Tags must be between 3 and 20 characters in length")
+
+        if len(self.data) > 8:
+            raise ValidationError("Writeups can have at most 8 tags")
 
 
 class WriteupForm(Form):
