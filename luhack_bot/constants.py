42ea09f67c35cc3d3cc3e6217593e3f9c9a606e7
Ben Simms <ben@bensimms.moe>
2019-08-26 23:09:09 +0100

    Make trusted roles a set

--- luhack_bot/constants.py
+++ luhack_bot/constants.py
@@ -7,11 +7,11 @@ verified_luhacker_role_id = 486249689050644480
 disciple_role_id = 506420426419732499
 furry_master_cyber_wizard_role_id = 502203959239245854
 master_cyber_wizard_role_id = 502197689434374144
-trusted_role_ids = (
+trusted_role_ids = {
     disciple_role_id,
     furry_master_cyber_wizard_role_id,
     master_cyber_wizard_role_id,
-)
+}
 bot_log_channel_id = 588443109994528816
 inner_magic_circle_id = 506447021780107275
 writeups_base_url = yarl.URL("https://scc-luhack.lancs.ac.uk/writeups")
