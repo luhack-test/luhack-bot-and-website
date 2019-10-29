41afb194d81328db3f80201b6dc40bca076e6df7
Ben Simms <ben@bensimms.moe>
2019-10-29 18:39:06 +0000

    Don't explode on an unknown language

--- luhack_site/markdown.py
+++ luhack_site/markdown.py
@@ -1,18 +1,25 @@
 import mistune
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
+from pygments.util import ClassNotFound
 from pygments.formatters import html
 
 
 class HighlightRenderer(mistune.Renderer):
     def block_code(self, code, lang):
-        if not lang:
+        def no_highlight():
             escaped = mistune.escape(code)
             return f"\n<pre><code>{escaped}</code></pre>\n"
 
-        lexer = get_lexer_by_name(lang, stripall=True)
-        formatter = html.HtmlFormatter()
-        return highlight(code, lexer, formatter)
+        if not lang:
+            return no_highlight()
+
+        try:
+            lexer = get_lexer_by_name(lang, stripall=True)
+            formatter = html.HtmlFormatter()
+            return highlight(code, lexer, formatter)
+        except ClassNotFound:
+            return no_highlight()
 
     def autolink(self, link, is_email=False):
         text = link = mistune.escape_link(link)
