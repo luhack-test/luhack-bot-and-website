4f11a802bdc25804505cd21d2f91905ac957b728
Ben Simms <ben@bensimms.moe>
2020-09-18 21:32:09 +0100

    Add caddy

--- start_site.sh
+++ start_site.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 /opt/poetry/bin/poetry run alembic upgrade head \
-    && /opt/poetry/bin/poetry run uvicorn --proxy-headers --host "0.0.0.0" --port 8080 luhack_site.site:app
+       && /opt/poetry/bin/poetry run uvicorn --proxy-headers \
+       --forwarded-allow-ips "*" \
+       --host "0.0.0.0" \
+       --port 8080 \
+       luhack_site.site:app
